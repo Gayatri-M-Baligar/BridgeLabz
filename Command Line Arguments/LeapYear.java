bc6ef52832706c26cf7bd268957e8fd5d7e31f75
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-22 12:51:24 +0530

    Leap Year or Not

---
+++ Command Line Arguments/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int year=sc.nextInt();
+       if(year%400==0){
+           System.out.println(year + " is leap year");
+       }else if(year%100==0){
+           System.out.println(year + " is not a leap year");
+       }else if(year%4==0){
+           System.out.println(year + " is leap year");
+       }else{
+           System.out.print(year + " Not a leap year");
+       }
+
+    }
+}
