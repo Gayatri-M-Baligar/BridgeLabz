5bbd31fbe0ab39e8ef623ca0becaebed5c8d9ceb
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-22 14:45:14 +0530

    Printing Day Of Given Particular Date

---
+++ Logical Programming/DayOfWeek.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class DayOfWeek {
+    static int dayOfWeek(int m,int d,int y){
+        int y0 = y - (14 - m) / 12;
+
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+
+        return d0;
+    }
+    public static void main(String[] args) {
+        int m=Integer.parseInt(args[0]);
+        int d=Integer.parseInt(args[1]);
+        int y=Integer.parseInt(args[2]);
+        int day=dayOfWeek(m,d,y);
+        switch(day){
+            case 0:
+                System.out.print("Sunday");
+                break;
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+        }
+
+    }
+}
