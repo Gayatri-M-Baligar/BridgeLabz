4d6005389a4dba29d4bf9b7803cbf76a3889b47f
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-22 14:35:01 +0530

    Display Unit,Tens,and Thousand placed digit

---
+++ AccessModifiers and Operators/DisplayPlaces.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DisplayPlaces {
+    public static void main(String[] args) {
+        long n=1101001000;
+        System.out.println("Unit Digit is: " + n%10);
+        System.out.println("Tens Digit is: " + n%100);
+        System.out.print("Thousands Digit is: " + n%1000);
+
+    }
+}
