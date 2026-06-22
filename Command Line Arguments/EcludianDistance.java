311005cb8e57a886d83027a6bb83f0309f8089ef
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-22 12:50:36 +0530

    Calculate Ecludian Distance

---
+++ Command Line Arguments/EcludianDistance.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EcludianDistance {
+    public static void main(String[] args) {
+        int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+        System.out.print(Math.sqrt(x*x + y*y));
+
+
+    }
+}
