07c63a80c2d91ec4ce07fc3d03235dd64a1e8376
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-24 16:41:49 +0530

    [Gayatri : Added welcome message for Employee Wage Computation Program in OOPs_Practice branch]

---
+++ EmployeeWageUC1.java
@@ -0,0 +1,8 @@
+package com.bridgeLabz.EmployeeProblem;
+
+public class EmployeeWageUC1 {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
