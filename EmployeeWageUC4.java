b9806ce4a084ee3ffc5fc8f4a39dc55a48dd02fa
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-24 17:29:07 +0530

    [Gayatri : UC3 - Added part-time employee wage calculation]

---
+++ EmployeeWageUC4.java
@@ -0,0 +1,18 @@
+package com.bridgeLabz.EmployeeProblem;
+
+public class EmployeeWageUC4 {
+    public static void main(String[] args) {
+
+        // Define wage per hour
+        int wagePerHour = 20;
+
+        // Define part-time working hours
+        int partTimeHours = 8;
+
+        // Calculate part-time employee wage
+        int empWage = wagePerHour * partTimeHours;
+
+        // Display part-time employee wage
+        System.out.println("Part Time Employee Wage: " + empWage);
+    }
+}
